9fac43e833a30cf26ebf48ac0de2fe2ae85ce192
MuhamedElsafy <muhamedd.kh@gmail.com>
2023-10-23 15:59:02 +0300

    contact page done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './Components/contact/contact.component';
+import { ServicesComponent } from './Components/services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"contact",component:ContactComponent},
+  {path:"services" ,component:ServicesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
